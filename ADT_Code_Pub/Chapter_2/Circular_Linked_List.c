885b474ec474e762d0f4216c0f4a0b3fbb6377d3
AugustusWillisWang <zeweiwang@outlook.com>
2018-03-27 21:15:34 +0800

    Rebuilt almost all funcs.

--- ADT_Code_Pub/Chapter_2/Circular_Linked_List.c
+++ ADT_Code_Pub/Chapter_2/Circular_Linked_List.c
@@ -28,10 +28,12 @@ typedef struct LNode_C *LinkList_C;
 //---------|FuncList|----------
 Status InitList_C(LinkList_C *L);
 //构造一个空的循环链表L
-Status DestroyList_C(LinkList_C L);
+Status DestroyList_C(LinkList_C *L);
 //销毁循环链表L
 void ClearList_C(LinkList_C L);
 //将L重置为空表
+Status ListAppend_C(LinkList_C L, ElemType i);
+//在L的开头插入元素i
 Status ListEmpty_C(LinkList_C L);
 //判断L是否为空表
 int ListLength_C(LinkList_C L);
@@ -50,7 +52,7 @@ Status ListInsert_C(LinkList_C L, int, ElemType);
 //在循环链表L中第i个位置之前插入新的元素e
 Status ListDelete_C(LinkList_C L, int, ElemType *);
 //在循环链表L中删除第i个元素，并用e返回其值
-void MergeList_C(LinkList_C L, LinkList_C Y, LinkList_C B, int (*compare)(ElemType, ElemType));
+void MergeList_C(LinkList_C L, LinkList_C Y, LinkList_C *B, int (*compare)(ElemType, ElemType));
 //已知循环链表La和Lb的元素按值非递减排列
 Status ListTraverse_C(LinkList_C L, Status (*visit)(ElemType *));
 //依次对L的每个数据元素调用函数visit()，一旦visit()失败，则操作失败
@@ -73,29 +75,25 @@ Status InitList_C(LinkList_C *L) //2.3
     return OK;
 } //构造一个空的循环链表L
 
-Status DestroyList_C(LinkList_C L)
+Status DestroyList_C(LinkList_C *L)
 {
     //输入参数是一个LinkList类型的变量L
     //函数将整个循环链表都free掉
-    LinkList_C temp, temp1, initial;
-    if (!L)
-        exit(OVERFLOW);
-    initial = L;
-    if (L->next == 0)
-        free(L);
-    for (temp = L->next; temp != initial;)
+    LinkList_C p;
+    if (!p)
+        exit(ERROR);
+    while (p = p->next)
     {
-        if (!temp)
-            exit(OVERFLOW);
-        temp1 = temp->next;
-        free(temp);
-        temp = temp1;
-    }
-    free(L);
-    L = NULL;
-    return OK;
-} //销毁循环链表L
-
+        free(p);
+        if (p == *L)
+        {
+            break;
+        }
+        free(*L);
+        *L = 0;
+        return OK;
+    } //销毁循环链表L
+}
 void ClearList_C(LinkList_C L)
 {
     //删除所有元素, 保留循环链表的空表头
@@ -165,31 +163,24 @@ Status GetElem_C(LinkList_C L, int i, ElemType *e)
 
 int compare(ElemType a, ElemType b)
 {
-    //两个参数都是元素类型，如果a大于b就输出1，等于输出0，小于输出-1
-    if (a == b)
-        return 0;
-    else if (a > b)
-        return 1;
-    else
-        return -1;
+    return a - b;
 } //元素的比较函数
 
 int LocateElem_C(LinkList_C L, ElemType e, int (*compare)(ElemType, ElemType))
 {
     //输入参数是一个循环链表L，一个用来比较的元素e，和用来比较的函数指针
     //返回值是一个int型的整数，是第一个满足和compare满足关系的元素序号
-    int i;
-    int n;
+    int i = 1;
     LinkList_C p = L;
     if (ListEmpty_C(L))
         return ERROR;
-    n = ListLength_C(L);
-    for (i = 1; compare(L->data, e) && i != n; i++, p = p->next)
-        ;
-    if (i == n)
-        return ERROR;
-    else
-        return i;
+    do
+    {
+        if (compare(e, p->data) == 0)
+            return i;
+        i++;
+    } while (L != (p = p->next));
+    return ERROR;
 } //在循环链循环链表L中查找第1个值与e满足compare()的元素的位序
 
 Status PriorElem_C(LinkList_C L, ElemType cur_e, ElemType *pre_e)
@@ -230,7 +221,7 @@ Status NextElem_C(LinkList_C L, ElemType cur_e, ElemType *next_e)
         return ERROR;
     else
     {
-        *next_e = temp->data;
+        *next_e = p->next->data;
         return OK;
     }
 }
@@ -273,35 +264,38 @@ Status ListInsert_C(LinkList_C L, int i, ElemType e)
 Status ListDelete_C(LinkList_C L, int i, ElemType *e)
 {
     //在循环表L中删除第i个元素，并用e返回其值
-    int j, n;
-    LinkList_C p, temp;
-    if (ListEmpty_C(L))
+    int len = ListLength_C(L);
+    if (!L || i < 1 || i > len || !L->next)
         return ERROR;
-    n = ListLength_C(L);
-    if(n==1)
-        L->next = 0;
-    p = L;
-    if (i > n)
-        return ERROR;
-    else if (i = 1)
+    if (i == 1)
     {
-        for (j = 1; j != n; i++)
-            p = p->next;
-        p->next = L->next;
         *e = L->data;
-        free(L);
-        L = p->next;
+        if (L->next!=L)
+        {
+            L->data = L->next->data;
+            LinkList_C t = L->next;
+            L->next = L->next->next;
+            free(t);
+            return OK;
+        }
+        L->next = 0;
         return OK;
     }
-    else
+    int cnt = 2;
+    LinkList_C now = L->next;
+    LinkList_C before = L;
+    while (cnt != i)
     {
-        for (j = 1; j != i; i++, p = p->next)
-            temp = p;
-        temp->next = p->next;
-        *e = p->data;
-        free(p);
-        return OK;
+        if (now == L && i != 1)
+            return ERROR;
+        cnt++;
+        now = now->next;
+        before = before->next;
     }
+    *e = now->data;
+    before->next = now->next;
+    free(now);
+    return OK;
 }
 //在循环表L中删除第i个元素，并用e返回其值
 
@@ -317,63 +311,81 @@ Status CreatList_C(LinkList_C *L, int n) //函数返回Status类型
     {
         ElemType a;
         scanf("%d", &a);
-        ListInsert_C(*L, 1, a);
+        ListAppend_C(*L, a);
     }
 } //ClearList_C
 
-void MergeList_C(LinkList_C La, LinkList_C Lb, LinkList_C Lc, int (*compare)(ElemType, ElemType))
+void MergeList_C(LinkList_C La, LinkList_C Lb, LinkList_C *Lc, int (*compare)(ElemType, ElemType))
 {
     //已知单循环链表La和Lb的元素按值非递减排列
     //归并La和Lb得到新的单循环链表Lc，Lc的元素也按值非递减排列
 
     if (ListEmpty_C(La))
     {
-        Lc = La;
+        *Lc = La;
         return;
     }
 
     if (ListEmpty_C(Lb))
     {
-        Lc = Lb;
+        *Lc = Lb;
         return;
     }
-    LinkList_C pa, pb, pc;
-    pa = La->next;
-    pb = Lb->next;
-    pc = La;
-    Lc = La; //用La的头结点作为Lc的头结点
-
-    while (pa != La && pb != Lb)
+    
+    int cnta=0;
+    int cntb=0;
+    int lena = ListLength_C(La);
+    int lenb = ListLength_C(Lb);
+    LinkList_C Lchead=(LinkList_C)malloc(sizeof(struct LNode_C));
+    (*Lc) = Lchead;
+    while ((cnta < lena) && (cntb < lenb))
     {
-        if (compare(pa->data, pb->data) < 0)
+        if (compare(La->data, Lb->data) < 0)
         {
-            pc->next = pa;
-            pc = pa;
-            pa = pa->next;
+            (*Lc)->next = La;
+            (*Lc) = La;
+            La = La->next;
+            cnta++;
         }
         else
         {
-            pc->next = pb;
-            pc = pb;
-            pb = pb->next;
+            (*Lc)->next = Lb;
+            (*Lc) = Lb;
+            Lb = Lb->next;
+            cntb++;
         }
     }
 
-    while (pa != La || pb != Lb)
-        pc->next = (pa != La) ? pa : pb;
+    if(cnta==lena)
+    {
+        (*Lc)->next = Lb;
+        do{
+            cntb++;
+            *Lc = (*Lc)->next;
+        } while (lenb != cntb);
+    }
+    else
+    {
+        (*Lc)->next = La;
+        do{
+            cnta++;
+            *Lc = (*Lc)->next;
+        } while (lena != cnta);
+    }
 
-    pc->next = Lc;
-
-    free(Lb);
+    (*Lc)->next = Lchead->next;
+    *Lc = Lchead->next;
+    free(Lchead);
     // return Lc;
 } //MergeList_C
 
 Status ListTraverse_C(LinkList_C L, Status (*visit)(ElemType *))
 {
     //依次对L的每个数据元素调用函数visit()，一旦visit()失败，则操作失败
-    LinkList_C p;
-    p = L->next;
     if (ListEmpty_C(L))
+        return ERROR;
+    LinkList_C p=L->next;
+    if(visit(&p->data) != OK)
         return ERROR;
     while (p != L && visit(&p->data) == OK)
         p = p->next;
@@ -386,104 +398,224 @@ Status ListTraverse_C(LinkList_C L, Status (*visit)(ElemType *))
 Status PrintList_C(LinkList_C L)
 {
     //打印单循环链表L
-    LinkList_C p;
-    p = L->next;
-    while (p != L)
+    if (!L)
+    {
+        puts("Print non-exist list.");
+        return ERROR;
+    }
+    if (!L->next)
+    {
+        puts("Print empty list.");
+        return ERROR;
+    }
+    LinkList_C p = L;
+    while (p && p->next)
     {
         printf("%d ", p->data);
         p = p->next;
+        if (p == L)
+            break;
     }
     printf("\n");
 } //PrintList_C
 
+Status ListAppend_C_old(LinkList_C L, ElemType i)
+{
+    if (!L)
+        exit(ERROR);
+    if (L->next)
+    {
+        LinkList_C t = (LinkList_C)malloc(sizeof(struct LNode_C));
+        if (!t)
+            exit(OVERFLOW);
+        t->next = L->next;
+        L->next = t;
+        t->data = i;
+        return OK;
+    }
+    L->next = L;
+    L->data = i;
+    return OK;
+}
+
+Status ListAppend_C(LinkList_C L, ElemType i)
+{
+    //注意: 由于特殊的结构定义, 这里的处理与正常插入有所区别, 但是复杂度基本不变
+    if (!L)
+        exit(ERROR);
+    if (L->next)
+    {
+        LinkList_C t = (LinkList_C)malloc(sizeof(struct LNode_C));
+        if (!t)
+            exit(OVERFLOW);
+        t->next = L->next;
+        t->data = L->data;
+        L->next = t;
+        L->data = i;
+        return OK;
+    }
+    L->next = L;
+    L->data = i;
+    return OK;
+}
+    // #include "lazy.h"
+    // int main()
+    // {
+
+    //     LinkList_C L;
+    //     CreatList_C(&L, 3);
+    //     int a;
+    //     ListInsert_C(L, 2, 33);
+    //     GetElem_C(L, 2, &a);
+    //     CK(a);
+    //     ListDelete_C(L, 1, &a);
+    //     CK(a);
+    //     LinkList_C LL;
+    //     CreatList_C(&LL, 2);
+    //     LinkList_C LLL;
+    //     MergeList_C(L, LL, LLL, compare);
+    //     LinkList_C Q;
+    //     InitList_C(&Q);
+    //     PrintList_C(Q);
+    //     DestroyList_C(L);
+    //     ClearList_C(Q);
+
+    //     a = ListEmpty_C(Q);
+    //     CK(a);
+    //     ListLength_C(LL);
+    //     CK(LocateElem_C(LL, 1, compare));
+
+    //     PriorElem_C(LL, 1, &a);
+    //     NextElem_C(LL, 1, &a);
+    //     Status mprint(int *t)
+    //     {
+    //         printf("%d ", *t);
+    //     }
+    //     ListTraverse_C(LL, mprint);
+    //     return 0;
+    // }
+
+    //完整版函数
+
+    // //---------|FuncList|----------
+    // //这里实现的是带头结点的线性链表, 这是完整的函数列表
+    // Status CreatList_C(LinkList_C L, int n);
+    // //逆位序输入n个元素的值，建立带表头节点的单链线性表L
+    // Status InitList_C(LinkList_C L);
+    // //构造一个空的线性链表
+    // Status DestroyList_C(LinkList_C L);
+    // //销毁线性链表L, L不再存在
+    // Status ClearList_C(LinkList_C L);
+    // //将线性链表置为空表并释放原链表的节点空间
+    // Status InsFirst_C(LinkList_C h,LinkList_C s);
+    // //已知h指向线性链表的头节点, 将s所指的节点插入到第一个节点之前
+    // Status DelFirst_C(LinkList_C h,LinkList_C q);
+    // //已知h指向线性链表的头节点, 删除链表中的第一个节点并以q返回
+    // Status Append_C(LinkList_C L,LinkList_C s);
+    // //将指针s所指的(彼此以指针相连的)一串节点连接在线性链表后的最后一个节点, 并改变链表L的尾指针指向新的尾节点.
+    // Status Remove_C(LinkList_C L, LinkList_C q);
+    // //删除线性链表L的尾节点并以q返回, 改变链表L的尾指针指向新的尾节点
+    // Status InsBefore_C(LinkList_C L, LinkList_C p, LinkList_C s);
+    // //已知p指向线性链表L中的一个节点, 将s插入到p所指的节点之前, 并修改指针p指向新插入的节点
+    // Status InsAfter_C(LinkList_C L, LinkList_C p, LinkList_C s);
+    // //已知p指向线性链表L中的一个节点, 将s插入到p所指的节点之后, 并修改指针p指向新插入的节点
+    // Status SetCurElem_C(LinkList_C p, ElemType e);
+    // //p指向线性链表中的一个节点, 用e更新其中数据元素的值
+    // ElemType GetCurElem_C(LinkList_C p);
+    // //p指向线性链表中的一个节点, 返回其中数据元素的值
+    // Status ListEmpty_C(LinkList_C L);
+    // //若线性链表L为空表返回TRUE, FALSE
+    // int ListLength_C(LinkList_C L);
+    // //返回线性链表L中元素个数
+    // Position GetHead_C(LinkList_C L);
+    // //返回线性链表L中头结点的位置
+    // Position GetLast_C(LinkList_C L);
+    // //返回线性链表L中最后一个结点的位置
+    // Position PriorPos_C(LinkList_C L, LinkList_C p);
+    // //已知p指向线性链表L中的一个结点，返回p所指结点的直接前驱的位置，
+    // //若无前驱，则返回NULL
+    // Position NextPos_C(LinkList_C L, LinkList_C p);
+    // //已知p指向线性链表L中的一个结点，返回p所指结点的直接后继的位置，
+    // //若无后继，则返回NULL
+    // Status LocatePos_C(LinkList_C L, int i, LinkList_C p);
+    // //返回p指示线性链表L中第i个结点的位置并返回OK，i值不合法时返回ERROR
+    // Position LocateElem_C(LinkList_C L, ElemType e, Status(*compare)(ElemType, ElemType));
+    // //返回线性链表L中第一个与e满足函数compare（）判定关系的元素的位置，
+    // //若不存在这样的元素，则返回NULL
+    // Status ListTraverse_C(LinkList_C L, Status (*visit)());
+    // //依次对L的每个元素调用函数visit（）。一旦visit（）失败，则操作失败。
+    // Status MergeList_C(LinkList_C La, LinkList_C Lb, LinkList_C Lc, int (*compare)(ElemType, ElemType));
+    // //已知单链线性表La和Lb的元素按值非递减排列
+    // //归并La和Lb得到新的单链线性表Lc，Lc的元素也按值非递减排列
+
+    //-----------------------------
+
+// for debug: 
 // #include "lazy.h"
+
 // int main()
 // {
+//     LinkList_C a;
+//     InitList_C(&a);
+//     ListAppend_C(a, 1);
+//     ListAppend_C(a, 2);
+//     ListAppend_C(a, 3);
+//     PrintList_C(a);
+//     CK(ListEmpty_C(a));
+//     CK(ListLength_C(a));
+//     int b;
+//     GetElem_C(a, 1, &b);
+//     CK(b);
+//     GetElem_C(a, 2, &b);
+//     CK(b);
+//     GetElem_C(a, 3, &b);
+//     CK(b);
+//     GetElem_C(a, 0, &b);
+//     CK(b);
+//     GetElem_C(a, 4, &b);
+//     CK(b);
+//     PrintList_C(a);
 
-//     LinkList_C L;
-//     CreatList_C(&L, 3);
-//     int a;
-//     ListInsert_C(L, 2, 33);
-//     GetElem_C(L, 2, &a);
-//     CK(a);
-//     ListDelete_C(L, 1, &a);
-//     CK(a);
-//     LinkList_C LL;
-//     CreatList_C(&LL, 2);
-//     LinkList_C LLL;
-//     MergeList_C(L, LL, LLL, compare);
-//     LinkList_C Q;
-//     InitList_C(&Q);
-//     PrintList_C(Q);
-//     DestroyList_C(L);
-//     ClearList_C(Q);
+//     CK(LocateElem_C(a, 1, compare));
+//     CK(LocateElem_C(a, 2, compare));
+//     CK(LocateElem_C(a, 3, compare));
+//     CK(LocateElem_C(a, 4, compare));
 
-//     a = ListEmpty_C(Q);
-//     CK(a);
-//     ListLength_C(LL);
-//     CK(LocateElem_C(LL, 1, compare));
+//     PriorElem_C(a, 2, &b);
+//     CK(b);
+//     b = 0;
+//     NextElem_C(a, 2, &b);
+//     CK(b);
+//     ListInsert_C(a, 1, 9);
+//     PrintList_C(a);
+//     ListDelete_C(a, 1, &b);
+//     CK(b);
+//     PrintList_C(a);
+//     ListDelete_C(a, 2, &b);
+//     CK(b);
+//     PrintList_C(a);
 
-//     PriorElem_C(LL, 1, &a);
-//     NextElem_C(LL, 1, &a);
-//     Status mprint(int *t)
-//     {
-//         printf("%d ", *t);
-//     }
-//     ListTraverse_C(LL, mprint);
-//     return 0;
+//     ClearList_C(a);
+//     CK(ListLength_C(a));
+//     PrintList_C(a);
+//     CK(ListEmpty_C(a));
+//     DestroyList_C(&a);
+//     CK(a);
+//     PrintList_C(a);
+//     ListInsert_C(a, 1, 9);
+//     PrintList_C(a);
+
+//     LinkList_C d;
+//     LinkList_C e;
+//     LinkList_C f;
+//     CreatList_C(&d,3);
+//     PrintList_C(d);
+//     CreatList_C(&e,3);
+//     PrintList_C(e);
+//     MergeList_C(e, d, &f, compare);
+//     PrintList_C(f);
+    
+
+//     // ListInsert_C(a, 1, 1);
+//     BP;
 // }
-
-//完整版函数
-
-// //---------|FuncList|----------
-// //这里实现的是带头结点的线性链表, 这是完整的函数列表
-// Status CreatList_C(LinkList_C L, int n);
-// //逆位序输入n个元素的值，建立带表头节点的单链线性表L
-// Status InitList_C(LinkList_C L);
-// //构造一个空的线性链表
-// Status DestroyList_C(LinkList_C L);
-// //销毁线性链表L, L不再存在
-// Status ClearList_C(LinkList_C L);
-// //将线性链表置为空表并释放原链表的节点空间
-// Status InsFirst_C(LinkList_C h,LinkList_C s);
-// //已知h指向线性链表的头节点, 将s所指的节点插入到第一个节点之前
-// Status DelFirst_C(LinkList_C h,LinkList_C q);
-// //已知h指向线性链表的头节点, 删除链表中的第一个节点并以q返回
-// Status Append_C(LinkList_C L,LinkList_C s);
-// //将指针s所指的(彼此以指针相连的)一串节点连接在线性链表后的最后一个节点, 并改变链表L的尾指针指向新的尾节点.
-// Status Remove_C(LinkList_C L, LinkList_C q);
-// //删除线性链表L的尾节点并以q返回, 改变链表L的尾指针指向新的尾节点
-// Status InsBefore_C(LinkList_C L, LinkList_C p, LinkList_C s);
-// //已知p指向线性链表L中的一个节点, 将s插入到p所指的节点之前, 并修改指针p指向新插入的节点
-// Status InsAfter_C(LinkList_C L, LinkList_C p, LinkList_C s);
-// //已知p指向线性链表L中的一个节点, 将s插入到p所指的节点之后, 并修改指针p指向新插入的节点
-// Status SetCurElem_C(LinkList_C p, ElemType e);
-// //p指向线性链表中的一个节点, 用e更新其中数据元素的值
-// ElemType GetCurElem_C(LinkList_C p);
-// //p指向线性链表中的一个节点, 返回其中数据元素的值
-// Status ListEmpty_C(LinkList_C L);
-// //若线性链表L为空表返回TRUE, FALSE
-// int ListLength_C(LinkList_C L);
-// //返回线性链表L中元素个数
-// Position GetHead_C(LinkList_C L);
-// //返回线性链表L中头结点的位置
-// Position GetLast_C(LinkList_C L);
-// //返回线性链表L中最后一个结点的位置
-// Position PriorPos_C(LinkList_C L, LinkList_C p);
-// //已知p指向线性链表L中的一个结点，返回p所指结点的直接前驱的位置，
-// //若无前驱，则返回NULL
-// Position NextPos_C(LinkList_C L, LinkList_C p);
-// //已知p指向线性链表L中的一个结点，返回p所指结点的直接后继的位置，
-// //若无后继，则返回NULL
-// Status LocatePos_C(LinkList_C L, int i, LinkList_C p);
-// //返回p指示线性链表L中第i个结点的位置并返回OK，i值不合法时返回ERROR
-// Position LocateElem_C(LinkList_C L, ElemType e, Status(*compare)(ElemType, ElemType));
-// //返回线性链表L中第一个与e满足函数compare（）判定关系的元素的位置，
-// //若不存在这样的元素，则返回NULL
-// Status ListTraverse_C(LinkList_C L, Status (*visit)());
-// //依次对L的每个元素调用函数visit（）。一旦visit（）失败，则操作失败。
-// Status MergeList_C(LinkList_C La, LinkList_C Lb, LinkList_C Lc, int (*compare)(ElemType, ElemType));
-// //已知单链线性表La和Lb的元素按值非递减排列
-// //归并La和Lb得到新的单链线性表Lc，Lc的元素也按值非递减排列
-
-//-----------------------------
